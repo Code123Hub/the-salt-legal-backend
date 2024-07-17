0d039b5d8079371224dcf1b849f0bb201231a9c1
CodeHub <161290048+Code123Hub@users.noreply.github.com>
2024-07-17 15:32:02 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,42 @@
+
+
+
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const route = require("./route/routes");
-const cors = require('cors');
-
 dotenv.config();
-const port = process.env.PORT || 5173;
-
+const port = process.env.PORT || 3001;
+mongoose.set("strictQuery", true);
 const app = express();
+const cors = require('cors')
 
-app.use(cors());
+app.use(cors())
+
+
 app.use(express.json());
 
-mongoose.connect(
-  "mongodb://localhost:27017/test",
-  { useNewUrlParser: true, useUnifiedTopology: true }
-)
-.then(() => {
-  console.log("MongoDB is connected");
-})
-.catch((err) => {
-  console.error("MongoDB connection error:", err.message);
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
 });
+
+mongoose
+  .connect(
+    "mongodb+srv://nehajaiswal:neha123@nehadb.pcorgpc.mongodb.net/legalbackend",
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    console.log("mongoDB is connected");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 app.use("/", route);
 
-app.listen(port, () => {
+app.listen(port,  () => {
   console.log(`App is running on port ${port}`);
 });
